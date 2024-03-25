aebd6db06301e6406298299e8c5f1ebdd5a4f86f
ankitsaini-github <ankit121saini@gmail.com>
2024-03-25 08:57:04 +0530

    integrated react-chat-engine and added custom feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+
 function App() {
   
   return (
-    <>
-      <div>
-        App
-      </div>
-    </>
+      <ChatEngine
+        height='100vh'
+        projectID='4ee0b371-9c6a-4199-9721-09a2a3f29dad'
+        userName='test@test'
+        userSecret='123123'
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
   )
 }
 
